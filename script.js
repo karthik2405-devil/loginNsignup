472c00da97951e8c3139e0a7296f60f569461d66
karthik2405 <56595280+karthik2405-devil@users.noreply.github.com>
2023-02-13 18:31:51 +0530

    storing multiple values in local storage

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// window.onload = function() {
+//     let form= document.getElementById('createAccount');
+//     form.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     const username = document.getElementById("signupUsername").value;
+//     const email = document.getElementById("signupEmail").value;
+//     const password = document.getElementById("signupPassword").value;
+//     localStorage.setItem("name", username);
+//     localStorage.setItem("email", email);
+//     localStorage.setItem("password", password);
+//     alert("your details are saved");
+// });
+// };
+
+
 function setForMessage(forElement,type,message){
     const messageElement=forElement.querySelector("form__message");
     messageElement.textContent=message;
@@ -11,15 +26,16 @@ function setInputError(inputElement,message){
 
 }
 function clearInputError(inputElement){
+
     inputElement.classList.remove("form__input--error");
     inputElement.parentElement.querySelector(".form__input-error-message").textContent=" ";
 }
-
+window.onload=function(){
+  
+}
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm=document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
-
-
     document.querySelector("#linkCreateAccount").addEventListener("click",e =>{
         e.preventDefault();
         loginForm.classList.add("form--hidden");
@@ -45,9 +61,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".form__input").forEach(inputElement =>{
         inputElement.addEventListener("blur",e=>{
-            if(e.target.id="signupUsername" && e.target.value.length > 0 && e.target.value.length< 10){
+            if(e.target.id=="signupUsername" && e.target.value.length > 0 && e.target.value.length< 10){
                 setInputError(inputElement,"Username must be at least 10 characters in length");
-
             }
 
         });
@@ -58,3 +73,107 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+
+window.onload = function() {
+  localStorage.clear()
+  let users=[]
+
+  if(localStorage.getItem('user')){
+    //parse to JSON object 
+    let usersArray=JSON.parse(localStorage.getItem('user')) || []
+    console.log(usersArray)
+    for(var i=0;i<usersArray.length;i++){
+      if(usersArray[i].username){
+        console.log(usersArray[i].username)
+      }
+    }
+
+    // document.getElementById('username').value=obj.username
+    // document.getElementById('password').value=obj.password
+
+  };
+
+  let form= document.getElementById('createAccount');
+  form.addEventListener('submit',(e)=>{
+    
+    // $("#result").empty()
+    e.preventDefault()
+
+    const username = document.getElementById("signupUsername").value;
+    const email = document.getElementById("signupEmail").value;
+    const password = document.getElementById("signupPassword").value;
+    let user={
+      username:username,
+      password:password,
+      email:email
+    }
+    users.push([user])
+    // users.forEach(user=>{
+    //   let res=`
+    //   <tr>
+    //     <td>${user.username}</td>
+    //     <td>${user.password}</td>
+    //     <td>${user.email}</td>
+    //   </tr>
+    //   `
+    //   $("#result").append(res)
+    //})
+    
+
+    //local Storage
+    localStorage.setItem('user',JSON.stringify(users))
+    console.log(JSON.stringify(users))
+    localStorage.setItem("name", username);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+      
+    alert("your details are saved");
+  });
+};
+
+let postData = () => {
+    window.addEventListener('load', function() {
+      let form = document.getElementById('createAccount');
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = document.getElementById("signupUsername").value;
+        const email = document.getElementById("signupEmail").value;
+        const password = document.getElementById("signupPassword").value;
+        localStorage.setItem("name", username);
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
+        alert("your details are saved");
+  
+        fetch("http://localhost:3001/UserData", {
+          method: "POST", 
+          body: JSON.stringify({
+            name: username,
+            email: email,
+            password:password
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(() => { 
+          console.log("User data was posted successfully");
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+        });
+      });
+    });
+  }
+
+
+document.getElementById("submitButton").addEventListener("click", postData);
+
+  
